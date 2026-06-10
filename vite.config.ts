c07218fd14c728c5ce4e683fc4d712b4396fd8ee
tasjen <tas.j@livinginsider.com>
2026-06-10 17:17:46 +0700

    Add React compiler

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,10 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({ babel: { plugins: [["babel-plugin-react-compiler", {}]] } }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
