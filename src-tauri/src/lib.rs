a1571661c6207995ab353ad0ebb9d380fbde1a96
tasjen <tas.j@livinginsider.com>
2026-06-12 00:25:56 +0700

    Format

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -354,7 +354,7 @@ async fn submit_task(
 
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
             let _ = app
                 .get_webview_window("main")
                 .expect("no main window")
